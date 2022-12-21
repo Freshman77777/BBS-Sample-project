d3374c790ec4c16bd7b3ed346b61b7e81db793b6
Freshman77777 <ehdwp6413@gmail.com>
2022-12-21 16:20:38 +0900

    day3

--- src/main/java/board/BoardController.java
+++ src/main/java/board/BoardController.java
@@ -14,12 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import db.BoardDao;
+import db.ReplyDao;
 
 /**
  * Servlet implementation class BoardController
  */
 @WebServlet({ "/board/list", "/board/search", "/board/write", "/board/update",
-			  "/board/detail", "/board/delete", "/board/deleteConfirm" })
+			  "/board/detail", "/board/delete", "/board/deleteConfirm",
+			  "/board/reply" })
 public class BoardController extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -27,7 +29,7 @@ public class BoardController extends HttpServlet {
 		String[] uri = request.getRequestURI().split("/");
 		String action = uri[uri.length - 1];
 		BoardDao dao = new BoardDao();
-		//ReplyDao rdao = new ReplyDao();
+		ReplyDao replyDao = new ReplyDao();
 		HttpSession session = request.getSession();
 		String sessionUid = (String) session.getAttribute("uid");
 		session.setAttribute("menu", "board");
@@ -42,7 +44,7 @@ public class BoardController extends HttpServlet {
 		switch(action) {
 		case "list":
 			int page = Integer.parseInt(request.getParameter("page"));
-			List<Board> list = dao.listUsers("title", "", page);
+			List<Board> list = dao.listBoard("title", "", page);
 			
 			session.setAttribute("currentBoardPage", page);
 			int totalBoardNo = dao.getBoardCount();
@@ -62,12 +64,16 @@ public class BoardController extends HttpServlet {
 		case "detail":
 			bid = Integer.parseInt(request.getParameter("bid"));
 			uid = request.getParameter("uid");
-			// 조회수 증가, 단 본인이 읽은 것은 제외
-			if (! uid.equals(sessionUid)) {
+			String option = request.getParameter("option");
+			// 조회수 증가. 단, 본인이 읽거나 댓글 작성후에는 제외. 
+			if (option == null && (!uid.equals(sessionUid))) {
 				dao.increaseViewCount(bid);
 			}
 			board = dao.getBoardDetail(bid);
 			request.setAttribute("board", board);
+			List<Reply> replyList = replyDao.getReplies(bid);
+			request.setAttribute("replyList", replyList);
+			
 			rd = request.getRequestDispatcher("/board/detail.jsp");
 			rd.forward(request, response);
 			break;
@@ -81,12 +87,53 @@ public class BoardController extends HttpServlet {
 				files = request.getParameter("files");
 				
 				board = new Board(sessionUid, title, content, files);
-				dao.insert(board);
+				dao.insertBoard(board);
 				response.sendRedirect("/bbs/board/list?page=1");
 			}
 			break;
 			
+		case "reply":
+			content = request.getParameter("content");
+			bid = Integer.parseInt(request.getParameter("bid"));
+			uid = request.getParameter("uid");					// 게시글의 uid
+			// 게시글의 uid와 댓글을 쓰려고 하는 사람의 uid가 같으면 isMine이 1
+			int isMine = (uid.equals(sessionUid)) ? 1 : 0;		
+			Reply reply = new Reply(content, isMine, sessionUid, bid);
+			replyDao.insert(reply);
+			dao.increaseReplyCount(bid);
+			response.sendRedirect("/bbs/board/detail?bid=" + bid + "&uid=" + uid + "&option=DNI");
+			break;
 			
+		case "delete":
+			bid = Integer.parseInt(request.getParameter("bid"));
+			response.sendRedirect("/bbs/board/delete.jsp?bid=" + bid);
+			break;
+			
+		case "deleteConfirm":
+			bid = Integer.parseInt(request.getParameter("bid"));
+			dao.deleteBoard(bid);
+			response.sendRedirect("/bbs/board/list?page=" + session.getAttribute("currentBoardPage"));
+			break;
+			
+		case "update":
+			if (request.getMethod().equals("GET")) {
+				bid = Integer.parseInt(request.getParameter("bid"));
+				board = dao.getBoardDetail(bid);
+				request.setAttribute("board", board);
+				rd = request.getRequestDispatcher("/board/update.jsp");
+				rd.forward(request, response);
+			} else {
+				bid = Integer.parseInt(request.getParameter("bid"));
+				uid = request.getParameter("uid");
+				title = request.getParameter("title");
+				content = request.getParameter("content");
+				files = request.getParameter("files");
+				
+				board = new Board(bid, title, content, files);
+				dao.updateBoard(board);
+				response.sendRedirect("/bbs/board/detail?bid=" + bid + "&uid=" + uid + "&option=DNI");
+			}
+			break;
 			
 		default:
 			System.out.println(request.getMethod() + " 잘못된 경로");
