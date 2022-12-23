109f26d8efcbd6a5f587d2ef0d14bf97b2e9dd55
Freshman77777 <ehdwp6413@gmail.com>
2022-12-23 14:52:47 +0900

    day5

--- src/main/java/board/BoardController.java
+++ src/main/java/board/BoardController.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
+import org.apache.catalina.tribes.util.Arrays;
+
 import db.BoardDao;
 import db.ReplyDao;
 import misc.JSONUtil;
@@ -41,6 +43,7 @@ public class BoardController extends HttpServlet {
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
 		String title = null, content = null, files = null, uid = null, today = null;
+		String jsonFiles = "";
 		int bid = 0, totalBoardNo = 0, totalPages = 0, page = 0;
 		Board board = null;
 		List<Board> list = null;
@@ -91,7 +94,7 @@ public class BoardController extends HttpServlet {
 				dao.increaseViewCount(bid);
 			}
 			board = dao.getBoardDetail(bid);
-			String jsonFiles = board.getFiles();
+			jsonFiles = board.getFiles();
 			if (!(jsonFiles == null || jsonFiles.equals(""))) {
 				JSONUtil json = new JSONUtil();
 				List<String> fileList = json.parse(jsonFiles);
@@ -107,7 +110,8 @@ public class BoardController extends HttpServlet {
 
 		case "write":
 			if (request.getMethod().equals("GET")) {
-				response.sendRedirect("/bbs/board/write.jsp");
+				response.sendRedirect("/bbs/board/write2.jsp");		// Editor version
+				// response.sendRedirect("/bbs/board/write.jsp");
 			} else {
 				/**  /board/fileupload 로 부터 전달된 데이터를 읽음 */
 				title = (String) request.getAttribute("title");
@@ -148,16 +152,41 @@ public class BoardController extends HttpServlet {
 			if (request.getMethod().equals("GET")) {
 				bid = Integer.parseInt(request.getParameter("bid"));
 				board = dao.getBoardDetail(bid);
+				
+				jsonFiles = board.getFiles();
+				if (!(jsonFiles == null || jsonFiles.equals(""))) {
+					JSONUtil json = new JSONUtil();
+					List<String> fileList = json.parse(jsonFiles);
+					session.setAttribute("fileList", fileList);
+				}
+				
 				request.setAttribute("board", board);
-				rd = request.getRequestDispatcher("/board/update.jsp");
+				rd = request.getRequestDispatcher("/board/update2.jsp");	// Editor version
+				// rd = request.getRequestDispatcher("/board/update.jsp");
 				rd.forward(request, response);
 			} else {
-				bid = Integer.parseInt(request.getParameter("bid"));
-				uid = request.getParameter("uid");
-				title = request.getParameter("title");
-				content = request.getParameter("content");
-				files = request.getParameter("files");
-
+				String bid_ = (String) request.getAttribute("bid");
+				bid = Integer.parseInt(bid_);
+				uid = (String) request.getAttribute("uid");
+				title = (String) request.getAttribute("title");
+				content = (String) request.getAttribute("content");
+				
+				List<String> listAdditionalFiles = (List<String>) session.getAttribute("fileList");
+				
+				String delName = (String) request.getAttribute("delFile");
+				
+				if (!(delName == null || delName.equals(""))) {
+					File delFile = new File("c:/Temp/upload/" + delName);
+					delFile.delete();
+					listAdditionalFiles.remove(delName);
+				}
+				JSONUtil json = new JSONUtil();
+				files = (String) request.getAttribute("files");		// FileUpload에서 넘어온 것
+				List<String> tmpList = json.parse(files);
+				for (String tmp: tmpList)
+					listAdditionalFiles.add(tmp);
+				files = json.stringify(listAdditionalFiles);
+				
 				board = new Board(bid, title, content, files);
 				dao.updateBoard(board);
 				response.sendRedirect("/bbs/board/detail?bid=" + bid + "&uid=" + uid + "&option=DNI");
